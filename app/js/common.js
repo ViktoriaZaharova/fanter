7572c2eac372764f75398db5cb43b801ac59db26
Viktoria <zaharova181114@gmail.com>
2022-02-16 19:38:43 +0200

    edit 6

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,8 @@
 const swiper = new Swiper('.swiper', {
     direction: 'vertical',
     loop: false,
-
+    slidesPerView: 1,
+    slidesPerGroup: 1,
     pagination: {
         el: '.pagination',
         type: 'bullets',
